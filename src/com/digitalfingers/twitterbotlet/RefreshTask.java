9522ab0a26f8e71dc34e86b8d350b6b2bdb5ee3c
Matt Shapiro <typorrhea@gmail.com>
2012-04-17 12:52:34 -0600

    Twitter search & sorting out some project dependency issues

--- src/com/digitalfingers/twitterbotlet/RefreshTask.java
+++ src/com/digitalfingers/twitterbotlet/RefreshTask.java
@@ -3,6 +3,7 @@ package com.digitalfingers.twitterbotlet;
 import java.util.TimerTask;
 
 import com.digitalfingers.twitterbotlet.twitter.TwitQueryBuilder;
+import com.digitalfingers.util.HttpUtil;
 
 public class RefreshTask extends TimerTask {
 
@@ -11,12 +12,11 @@ public class RefreshTask extends TimerTask {
 			// search for query
 			TwitQueryBuilder queryBuilder = new TwitQueryBuilder();
 			String url = queryBuilder.buildWithTerm("surely");
-			GetMethod method = new GetMethod();
+			String response = HttpUtil.getResponseAsString(url);
 			// validate oauth
 			// loop n query matches
 				// parse match
 				// construct response
 				// update with @reply
 		}
-		
 }
---
+++ src/com/digitalfingers/util/HttpUtil.java
@@ -0,0 +1,45 @@
+package com.digitalfingers.util;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class HttpUtil {
+
+	public static String getResponseAsString(String url) {
+		HttpClient httpclient = new DefaultHttpClient();
+		String responseBody = null;
+		try {
+            HttpGet httpget = new HttpGet(url);
+
+            System.out.println("executing request " + httpget.getURI());
+
+            // Create a response handler
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            System.out.println("----------------------------------------");
+
+        } catch (ClientProtocolException e) {
+			System.out.println("ClientProtocolException " + e.getMessage());
+        	e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            httpclient.getConnectionManager().shutdown();
+        }
+		
+		return responseBody;
+	}
+	
+}
